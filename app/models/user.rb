498ded62ba016e34e05be3fbb9352111ef3b9eba
Paul Manein <manein.paul@gmail.com>
2024-05-28 16:32:48 +0200

    reservation backend

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,4 @@ class User < ApplicationRecord
 
   validates :email, presence: true, uniqueness: true
   validates :password, presence: true, length: { minimum: 6 }, on: :create
-  validates :first_name, presence: true
-  validates :last_name, presence: true
 end
