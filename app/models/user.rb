1457235fda9b111d3bde8a3225dfa2dd7c5ed4ff
Lucie Durak <duraklucie@gmail.com>
2024-05-28 16:32:49 +0200

    WIP: see-a-specific-item

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,5 @@ class User < ApplicationRecord
   has_many :resevations
 
   validates :email, presence: true, uniqueness: true
-  validates :password, presence: true, length: { minimum: 6 }, on: :create
-  validates :first_name, presence: true
-  validates :last_name, presence: true
+  # validates :password, presence: true, length: { minimum: 6 }, on: :create
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,43 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+puts 'destroying database'
+Item.destroy_all
+User.destroy_all
+
+puts 'Creating Items...'
+user1 = User.create!(
+  email: "test@gmail.com",
+  username: "test",
+  password: "testpassword"
+)
+
+user2 = User.create!(
+  email: "toto@gmail.com",
+  username: "toto",
+  password: "totopassword"
+)
+
+scie_circulaire = Item.new(
+  category: "Outils de jardinage",
+  name: "Scie Circulaire",
+  picture: "scie_circulaire.jpg",
+  price: 15.00,
+  description: "Scie circulaire performante pour tous vos travaux de découpe.",
+  address: "123 Rue des Jardins, 40150 Hossegor",
+  user: user1
+)
+scie_circulaire.save!
+
+perceuse = Item.new(
+  category: "Outils de jardinage",
+  name: "Perceuse",
+  picture: "perceuse.jpg",
+  price: 10.00,
+  description: "Perceuse robuste et facile à utiliser pour tous vos projets de bricolage.",
+  address: "123 Rue des Jardins, 40150 Hossegor",
+  user: user2
+)
+scie_circulaire.save!
+puts 'Finished!'
