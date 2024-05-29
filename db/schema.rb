faec6334ac077323faa72d0263798e342a3ad771
Lucie Durak <duraklucie@gmail.com>
2024-05-29 16:14:22 +0200

    ajout images seed

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema[7.1].define(version: 2024_05_29_094710) do
+ActiveRecord::Schema[7.1].define(version: 2024_05_29_140510) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -46,7 +45,6 @@ ActiveRecord::Schema[7.1].define(version: 2024_05_29_094710) do
   create_table "items", force: :cascade do |t|
     t.string "category"
     t.string "name"
-    t.string "picture"
     t.decimal "price"
     t.text "description"
     t.string "address"
